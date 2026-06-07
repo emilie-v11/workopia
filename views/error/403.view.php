963e399f338ae9aff8df0ac7548feec351b63719
Vukovic Emilie <emilievukovic79@gmail.com>
2026-06-07 21:05:13 +0200

    add 403 unauthorized page

---
+++ views/error/403.view.php
@@ -0,0 +1,15 @@
+<?php loadPartial('head'); ?>
+<div style="min-height:100vh; display:flex; flex-direction:column;">
+  <?php loadPartial('header'); ?>
+
+  <section class="flex-1 flex items-center justify-center" style="flex:1; display:flex; align-items:center; justify-content:center;">
+    <div class="container mx-auto p-4">
+      <div class="text-center text-3xl mb-4 font-bold border border-gray-300 p-3">403 Error</div>
+      <p class="text-center text-2xl mb-4">
+        You are not authorized to access this page
+      </p>
+    </div>
+  </section>
+</div>
+
+<?php loadPartial('footer'); ?>
